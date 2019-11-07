e815b17fbf73a5376f53a95949c43beb5c1958f6
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-08 00:08:42 +0100

    Create 16. Public.cpp

---
+++ OOP Wykłady 2016/16. Public.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+class foobar{
+public:
+  int foo;
+  int fun(){return foo1; }
+private:
+  int foo1;
+  int fun1(){
+    return this->foo;
+  }
+};
+
+int main()
+{
+  foobar fu;
+  fu.foo = 5; // works
+  //fu.foo1= 5; // doesn't
+  int a = fu.fun();// works
+  //a = fu.fun1();// doesn't
+
+  foobar *p = new foobar;
+  p->foo = 5; // works
+  //p->foo1= 5; // doesn't
+  int b = p->fun();// works
+  //b = p->fun1();// doesn't
+
+  delete p;
+}
+
