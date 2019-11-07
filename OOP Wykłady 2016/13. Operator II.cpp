cc545d5cd773a6a2c2d9ee5ed324cb786e0f38b3
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-07 23:57:37 +0100

    Create 13. Operator II.cpp

---
+++ OOP Wykłady 2016/13. Operator II.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class foo{
+  public:
+  int a;
+};
+void operator+(foo& a, foo& b) 
+{
+  a.a = a.a + b.a;
+  cout << "Wywołano operator + a=" << a.a << endl;
+}
+
+int main()
+{
+  foo f1, f2;
+  f1.a = 9;
+  f2.a = 3;
+  cout << f1.a << endl;
+  f1+f2;
+  cout << f1.a << endl;
+}
