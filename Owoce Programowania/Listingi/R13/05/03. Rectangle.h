130b2821488aa781aa87a12170c0df0206004da1
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-26 22:57:12 +0100

    Create 03. Rectangle.h

---
+++ Owoce Programowania/Listingi/R13/05/03. Rectangle.h
@@ -0,0 +1,24 @@
+// Specyfikacja klasy Rectangle
+// W tej werji wykorzystane s¹ œródwierszowe funkcje cz³onkowskie.
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+class Rectangle
+{
+  private:
+    double width;
+    double length;
+  public:
+    void setWidth(double);
+    void setLength(double);
+
+    double getWidth() const
+      { return width; }
+
+    double getLength() const
+      { return length; }
+
+    double getArea() const
+      { return width * length; }
+};
+#endif
