48a750717ae07fa71b4c6be779005825bf4f83b2
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-20 01:02:59 +0100

    Create 06. Main.cpp

---
+++ Owoce Programowania/Listingi/R19/06. Main.cpp
@@ -0,0 +1,34 @@
+// Ten program demonstruje użycie adaptera
+// kontenera stack.
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int main()
+{
+  const int MAX = 8;   // Maksymalna wartość do umieszczenia na stosie
+  int count;           // Licznik obiegów pętli
+
+  // Definicja stosu
+  stack< int, vector<int> > iStack;
+
+  // Odłożenie wartości na stosie
+  for (count = 2; count < MAX; count += 2)
+  {
+    cout << "Odkładanie wartości " << count << endl;
+    iStack.push(count);
+  }
+
+  // Wyświetlenie wielkości stosu
+  cout << "Wielkość stosu: ";
+  cout << iStack.size() << endl;
+
+  // Zdjęcie wartości ze stosu
+  for (count = 2; count < MAX; count += 2)
+  {
+    cout << "Zdejmowanie wartości " << iStack.top() << endl;
+    iStack.pop();
+  }
+  return 0;
+}
